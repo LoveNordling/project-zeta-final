53abab9a78159da64f1cd5acca2489fb04265a75
daha2574 <David.Hakansson.2574@student.uu.se>
2018-04-18 07:41:28 -0700

    Added some animals

---
+++ src/org/primal/java/entity/Giraffe.java
@@ -0,0 +1,8 @@
+package org.primal.java.entity;
+
+public class Giraffe extends Animal{
+    public Giraffe(float x, float y, float stam, float fullns){
+        super(x, y, stam, fullns);
+        starvationRate = 1;
+    }
+}
---
+++ src/org/primal/java/entity/Hyena.java
@@ -0,0 +1,8 @@
+package org.primal.java.entity;
+
+public class Hyena extends Animal{
+    public Hyena(float x, float y, float stam, float fullns){
+        super(x, y, stam, fullns);
+        starvationRate = 1;
+    }
+}
