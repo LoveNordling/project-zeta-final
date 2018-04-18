f0f7e165f8720829e05d6f4aaee8e7d8bf9775c8
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-04-18 16:53:25 +0200

    fix

--- src/org/primal/java/entity/Zebra.java
+++ src/org/primal/java/entity/Zebra.java
@@ -1,8 +1,13 @@
 package org.primal.java.entity;
 
-public abstract class Zebra extends Animal {
+public class Zebra extends Animal {
     public Zebra(float x, float y, float stamina, float fullness) {
         super(x, y, stamina, fullness);
         starvationRate = 1;
+    }
+
+    @Override
+    public void eat(LivingEntity food) {
+
     }
 }
