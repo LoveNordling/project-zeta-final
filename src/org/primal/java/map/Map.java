b8ee1a3cc366d1cd699d697c844f9711e35304a1
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-04-18 15:43:09 +0200

    minor changes

--- src/org/primal/java/map/Map.java
+++ src/org/primal/java/map/Map.java
@@ -1,4 +1,5 @@
 package org.primal.java.map;
+
 import org.primal.java.tile.Tile;
 
 import java.util.LinkedList;
@@ -11,8 +12,7 @@ public abstract class Map {
         chunks.add(new Chunk<>(0, 0));
     }
 
-    Tile getTile(float x, float y) {
-        return chunks.get(0).getTile(x, y);
+    public LinkedList getChunks() {
+        return chunks;
     }
-
 }
