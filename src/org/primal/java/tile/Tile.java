67a23ac20c17ec566c40535be8cfe68d6adf8bc9
huntexD <axel.hultin@gmail.com>
2018-04-18 20:39:05 +0200

    Main class must be static + fixed a nasty NULLPointerException

--- src/org/primal/java/tile/Tile.java
+++ src/org/primal/java/tile/Tile.java
@@ -4,14 +4,30 @@ import org.primal.java.SimObject;
 import org.primal.java.entity.LivingEntity;
 
 import java.util.List;
+import java.util.LinkedList;
 
 
 public class Tile extends SimObject {
     private List<LivingEntity> livingEntities;
 
+    public Tile(float x, float y){
+    	super(x,y);
+    	this.livingEntities = new LinkedList<LivingEntity>();
+    }
+
     public Tile(float x, float y, List<LivingEntity> livingEntities) {
         super(x, y);
         this.livingEntities = livingEntities;
+    }
+
+    public void addLivingEntity(LivingEntity ent){
+    	this.livingEntities.add(ent);
+    }
+
+    public void removeLivingEntity(LivingEntity ent){
+    	if(this.livingEntities.contains(ent)){
+    		this.livingEntities.remove(ent);
+    	}
     }
 
     public List<LivingEntity> getLivingEntities() {
