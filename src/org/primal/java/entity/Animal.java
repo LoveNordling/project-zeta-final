5199a22a0bc75f11ad13ed891f2545bb78f99136
Viktor Enzell <viktor.enzell@gmail.com>
2018-04-18 16:31:55 +0200

    Added lion class and random movement

--- src/org/primal/java/entity/Animal.java
+++ src/org/primal/java/entity/Animal.java
@@ -1,6 +1,7 @@
 package org.primal.java.entity;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 import org.primal.java.behaviour.Behaviour;
 import org.primal.java.responses.BehaviourResponse;
@@ -10,16 +11,30 @@ public abstract class Animal extends LivingEntity {
     protected float stamina;
     protected float fullness; //0-100
 
-    public Animal(float x, float y, float stam, float fullns){
-        super(x,y);
-        this.stamina = stam;
-        this.fullness = fullns;
+    public Animal(float x, float y, float stamina, float fullness) {
+        super(x, y);
+        this.stamina = stamina;
+        this.fullness = fullness;
     }
-    
-    public Animal(float x, float y){
-        this(x,y, 100, 100);
-        
+
+    public Animal(float x, float y) {
+        this(x, y, 100, 100);
     }
-    
+
+    // Temporary function for random movement
+    public void move() {
+        Random rand = new Random();
+        int n = rand.nextInt(3);
+        if (n == 0) {
+            position[0] += 1;
+        } else if (n == 1) {
+            position[0] -= 1;
+        } else if (n == 2) {
+            position[1] += 1;
+        } else {
+            position[1] -= 1;
+        }
+    }
+
     public abstract void eat(LivingEntity food);
 }
