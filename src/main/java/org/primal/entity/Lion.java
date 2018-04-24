2b6e041c9f4821a3d78bb76619156d52301c338a
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-04-24 12:03:24 +0200

    implemented a more correct constructor for Animal

--- src/main/java/org/primal/entity/Lion.java
+++ src/main/java/org/primal/entity/Lion.java
@@ -7,19 +7,18 @@ import org.primal.behaviour.Behaviour;
 import org.primal.behaviour.SearchFoodBehaviour;
 import org.primal.map.Map;
 
-
 import java.util.LinkedList;
 
 public class Lion extends Animal {
     public Lion(float x, float y, float stamina, float fullness, Map map) {
-        super(x, y, stamina, fullness, new Circle(x, y, 2, Color.ORANGE));
+        super(x, y, 100, stamina, fullness, new Circle(x, y, 2, Color.ORANGE));
         Behaviour foodBehaviour = new SearchFoodBehaviour(this, map);
         this.behaviours = new LinkedList<>();
         this.behaviours.add(foodBehaviour);
         this.starvationRate = 1;
     }
 
-    public void eat(LivingEntity food){
+    public void eat(LivingEntity food) {
 
     }
 }
