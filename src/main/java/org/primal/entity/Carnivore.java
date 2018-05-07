072eb0496737ff5146a50085da144148135b71ee
dannebra <dannebra92@gmail.com>
2018-05-07 11:16:15 +0200

    More work

--- src/main/java/org/primal/entity/Carnivore.java
+++ src/main/java/org/primal/entity/Carnivore.java
@@ -1,4 +1,40 @@
 package org.primal.entity;
 
-public class Carnivore {
+import org.primal.map.Map;
+import org.primal.tile.Tile;
+import org.primal.map.Chunk;
+
+import java.util.ArrayList;
+import java.sql.SQLOutput;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public abstract class Carnivore extends Animal {
+
+
+    public Carnivore(float x, float y, Map map, float health, float stamina, float fullness) {
+        super(x, y, map, health, fullness, stamina);
+    }
+    public void eat(LivingEntity food) {
+        if (food instanceof Herbivore) {
+            Tile tile = map.getTile(food.getX(), food.getY());
+            tile.removeLivingEntity(food);
+            this.stamina = getStamina() + 10;
+        }
+    }
+
+    public void simulate() {
+        move();
+    }
+
+    public void chase(Herbivore target) {
+
+        float dx = target.getX() - this.getX();
+        float dy = target.getY() - this.getY();
+
+        float newX = getX() + dx * 0.1f;
+        float newY = getY() + dy * 0.1f;
+
+        this.position.setLocation(newX, newY);
+    }
 }
+
