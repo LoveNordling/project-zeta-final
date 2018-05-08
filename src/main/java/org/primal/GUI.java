22a8fcdcc380167d8997464c7cde351b69fab97c
daha2574 <David.Hakansson.2574@student.uu.se>
2018-05-09 01:02:10 +0200

    some documentation added

--- src/main/java/org/primal/GUI.java
+++ src/main/java/org/primal/GUI.java
@@ -24,7 +24,7 @@ class Surface extends JPanel implements MouseListener, KeyListener {
     private float convertionRate;
     private boolean commandSent = false;
     private boolean inputMode = false;
-    public enum Commands{ NOTHING, SPAWNLIONS, PRINTALL, HEJ, KILL, KILLALL, KILLSOME, HEAL, RESPAWN, MASSHEAL, INPUT, LISTCOMMANDS }
+    public enum Commands{ NOTHING, SPAWNLIONS, PRINTALL, HEJ, KILL, KILLALL, KILLSOME, HEAL, RESPAWN, MASSHEAL, INPUT, LISTCOMMANDS, FREEZECHUNK, SPAWNANIMAL, SPAWNZEBRA, SPAWNHYENA }
     private Commands command;
     public Surface(Map map) {
         super();
@@ -36,7 +36,10 @@ class Surface extends JPanel implements MouseListener, KeyListener {
         System.out.println(this.isFocusable());
         this.map = map;
     }
-
+    /* doDrawing generates the graphical representation of the simulation
+     *
+     * @param Graphics g 
+     */
     private void doDrawing(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
@@ -77,43 +80,69 @@ class Surface extends JPanel implements MouseListener, KeyListener {
         super.paintComponent(g);
         doDrawing(g);
     }
-
+    /* translate translates the x and y coords of the graphical component to its backend representation 
+     *
+     * @param x the x coordinate generated from the map
+     * @param y the y coordinate generated from the map
+     * @return reterns the coordinate for the backend position choosen on the frontend graphics
+     */
     private Float translate(int x, int y) {
         float fX = x * convertionRate;
         float fY = y * convertionRate;
         return new Float(fX, fY);
     }
+    /* listCommands is a function used to send out the availible commands to the terminal
+     */
     private void listCommands(){
         //TODO add alot of print statements
         //TODO if possible add some kind of command listening in window
         System.out.println("The first of many print statements");
     }
+    /* printAll prints out inforamtion about all the living entites 
+     */
     private void printAll(){
         map.printAll();
     }
+    /* healAnimals heals the animals in the the tile with the position pos
+     *
+     * @param pos is the position of the tile to have it's inhabitats healed 
+     */
     private void healAnimals(Float pos){
         System.out.println("Thoughts and prayers sent!");
         Tile t = map.getTile(((float) pos.getX()), ((float) pos.getY()));
         t.antiSlaughter();
     }
+    /* massHeal a function for healing all the animals on the map
+     */
     private void massHeal(){
         System.out.println("Useless func");
         map.antiNuke();
     }
+    /* killSomeAnimals a function for killing a random amount of animals on the tile with position pos, the random amount is between 0 and the amount of animals existing on the tile
+     */
     private void killSomeAnimals(Float pos){
         System.out.println("This is a christian minecraft server: No killing allowed");
     }
+    /* killAnimals kills all the animals on the tile with the position pos
+     */
     private void killAnimals(Float pos){
         Tile t = map.getTile(((float) pos.getX()), ((float) pos.getY()));
         t.slaughter();
     }
+    /* NUKEDECIMATESLAUGHTER removes all existing beings on the map from existance
+     */
     private void NUKEDECIMATESLAUGHTER(){
         System.out.println("Freedom sent");
         map.nuke();
     }
+    /* spawn respawns a new set of beings, genarly used in conjunction with the inevitble destruction of all things living
+     */
     private void spawn(){
         System.out.println("Due to reasons this function is not implemented");
     }
+    /* input opens a window that that runs diffrent commands depending on input, type l/list commands/list to see all commands
+     * 
+     */
     private void input(){
         boolean exit = false;
         
@@ -154,6 +183,9 @@ class Surface extends JPanel implements MouseListener, KeyListener {
 
         inputMode = false;
     }
+    /* execCommands runs the currently active command if it is a zero arguments command
+     *
+     */
     private void execCommands(){
         switch(command){
             case INPUT:
@@ -179,7 +211,10 @@ class Surface extends JPanel implements MouseListener, KeyListener {
                 break;
         }
     }
-
+    /* execCommands runs the currently active command if it is a 1 argument command
+     *
+     * @param pos the argument to be sent onward to the function coresponding to the command 
+     */
     private void execCommands(Float pos){
         switch(command){
             case SPAWNLIONS:
@@ -196,7 +231,12 @@ class Surface extends JPanel implements MouseListener, KeyListener {
                 break;
        
         }
-    }
+    }/* mouseClicked is a function that is run when a click on the screen is received
+      *
+      * this function will either print out inforamtion about the living entities on the clicked tile or perform an action dependant on the command on it
+      *
+      * @param click inforamtion generated from the mouse click
+      */
     public void mouseClicked(MouseEvent click) {
         this.requestFocusInWindow();
         int x = click.getX();
@@ -218,6 +258,13 @@ class Surface extends JPanel implements MouseListener, KeyListener {
             input();
         }
     }
+    /* keyPressed sets the active command depending on which key is pressed, if the corresponding command is a zero argument command the active command is executed
+     *
+     * to see list of commands type click l or use the command box
+     * the key used to open the command box is 'i'
+     *
+     * @param e inforamtion about the key pressed
+     */
     public void keyPressed(KeyEvent e){
         int key = e.getKeyCode();
         
@@ -291,6 +338,10 @@ public class GUI extends JFrame {
         initUI(map);
     }
 
+    /* initUI initiates the graphical interface 
+     *
+     * @param map the map to be drawn/grapicaly shown
+     */
     private void initUI(Map map) {
         add(new Surface(map));
         setTitle("Primal");
