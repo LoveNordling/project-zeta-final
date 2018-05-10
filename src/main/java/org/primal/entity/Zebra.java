1977451e8951cd910664e2742a0e9b687894c6cc
daha2574 <David.Hakansson.2574@student.uu.se>
2018-05-10 12:32:09 +0200

    Zebra shoukd now work aswell

--- src/main/java/org/primal/entity/Zebra.java
+++ src/main/java/org/primal/entity/Zebra.java
@@ -1,6 +1,7 @@
 package org.primal.entity;
 
 import org.primal.behaviour.Behaviour;
+import org.primal.behaviour.BreedingBehaviour;
 import org.primal.behaviour.SearchFoodBehaviour;
 import org.primal.map.Map;
 import org.primal.tile.Tile;
@@ -22,11 +23,11 @@ public class Zebra extends Herbivore {
 
     public Zebra(float x, float y, Map map, float stamina, float fullness) {
         super(x, y, map, 100, stamina, fullness);
-
         Behaviour foodBehaviour = new SearchFoodBehaviour(this, map);
-        this.behaviours = new LinkedList<Behaviour>();
+        Behaviour breedBehaviour = new BreedingBehaviour(this, map);
+        this.behaviours = new LinkedList<>();
         this.behaviours.add(foodBehaviour);
-        this.starvationRate = 1;
+        this.behaviours.add(breedBehaviour);
     }
     public String getType (){
         return "Zebra";
