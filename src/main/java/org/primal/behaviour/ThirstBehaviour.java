0c76bcd2f008084337bda3916879109d31326e37
dannebra <dannebra92@gmail.com>
2018-05-14 10:34:17 +0200

    initial work

---
+++ src/main/java/org/primal/behaviour/ThirstBehaviour.java
@@ -0,0 +1,10 @@
+package org.primal.behaviour;
+
+import org.primal.tile.WaterTile;
+
+public class ThirstBehaviour extends Behaviour {
+    WaterTile selectTile;
+    boolean decided = false;
+
+    
+}
