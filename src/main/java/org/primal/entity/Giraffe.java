c7bc7d3a1580f0508c8a7f53e14b20f71c2f3de1
freddedotme <github@fredde.me>
2018-05-16 10:40:16 +0200

    changed moving speed

--- src/main/java/org/primal/entity/Giraffe.java
+++ src/main/java/org/primal/entity/Giraffe.java
@@ -1,6 +1,10 @@
 package org.primal.entity;
 
-import org.primal.behaviour.*;
+import org.primal.behaviour.Behaviour;
+import org.primal.behaviour.BreedingBehaviour;
+import org.primal.behaviour.FeedingBehaviour;
+import org.primal.behaviour.SearchFoodBehaviour;
+import org.primal.behaviour.ThirstBehaviour;
 import org.primal.map.Map;
 import org.primal.tile.Tile;
 
@@ -11,12 +15,13 @@ public class Giraffe extends Herbivore {
     /**
      * Creates a Giraffe object
      * Creates a new herbivore object
+     *
      * @param x        = x-coordinate
      * @param y        = y-coordinate
      * @param map      = current Map
      * @param stamina  = stamina points
      * @param fullness = fullness points
-     * @param thirst = thirst level
+     * @param thirst   = thirst level
      */
 
     public Giraffe(float x, float y, Map map, double stamina, double fullness, double thirst) {
@@ -35,21 +40,23 @@ public class Giraffe extends Herbivore {
 
         this.color = new java.awt.Color(255, 251, 0);
         this.shapeSize = Tile.getSize() / 2.5f;
+
+        speed = 0.02;
     }
-    
+
     /**
-     *getType gets the type of the animal
+     * getType gets the type of the animal
      *
-     * @return a string representing the type of the animal 
+     * @return a string representing the type of the animal
      */
-    public String getType (){
+    public String getType() {
         return "Giraffe";
     }
-    
+
     /**
-     *breed creates a new hyena at this hyena's tile
+     * breed creates a new hyena at this hyena's tile
      */
-    public void breed(){
+    public void breed() {
         Tile t = map.getTile(this.getX(), this.getY());
         map.spawnGiraffe(t);
     }
